e58ce20b1e51caa8128cf84207410ee1d1bfb70b
Liu Zhongnuo <2313678365@qq.com>
2024-03-04 19:31:22 +0800

    feat: 引入ant-design-vue以及pro-layout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+import "@ant-design-vue/pro-layout/dist/style.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import ProLayout, { PageContainer } from "@ant-design-vue/pro-layout";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Antd)
+  .use(ProLayout)
+  .use(PageContainer)
+  .mount('#app')
