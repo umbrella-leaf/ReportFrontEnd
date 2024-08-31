7dae0308dc94154e2a5ba9a99901e24370170cb3
umbrella-leaf <2313678365@qq.com>
2024-08-31 23:38:06 +0800

    feat: 增加工具函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,18 +4,19 @@ import {segment_concat} from "./segment_concat";
 const AppendixType = {
     IMAGE: 'image',
     VIDEO: 'video',
-    REPLY: 'reply'
+    REPLY: 'reply',
+    FORWARD: 'forward'
 }
 
 const AppendixTypeToButtonName = {
     [AppendixType.IMAGE]: '查看图片',
     [AppendixType.VIDEO]: '查看视频',
-    [AppendixType.REPLY]: '查看回复'
+    [AppendixType.REPLY]: '查看回复',
+    [AppendixType.FORWARD]: '查看转发消息'
 }
 
 function get_resource_suffix(url) {
     const parts = url.split(".");
-    console
     return parts[parts.length - 1];
 }
 
@@ -23,5 +24,18 @@ function get_button_name_from_appendix_type(url) {
     return AppendixTypeToButtonName[get_resource_suffix(url)];
 }
 
+function fetch_resources(url) {
+    return fetch(url).then((response) => {
+        return response.json()
+    });
+}
 
-export {parse_emoji, segment_concat, AppendixType, get_button_name_from_appendix_type, get_resource_suffix};
+
+export {
+    parse_emoji,
+    segment_concat,
+    AppendixType,
+    get_button_name_from_appendix_type,
+    get_resource_suffix,
+    fetch_resources
+};
